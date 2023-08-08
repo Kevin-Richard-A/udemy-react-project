5cd01177096f3cc51be222b2a29ed60ccf36fd37
Kevin-Richard-A <75072776+Kevin-Richard-A@users.noreply.github.com>
2023-08-09 02:16:43 +0530

    update to expenses.js

--- src/Components/Expenses/Expenses-List/Expenses.js
+++ src/Components/Expenses/Expenses-List/Expenses.js
@@ -12,9 +12,24 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   }
   console.log(filteredYear);
+  
+  let filterInfoText = '2019, 2020, 2021, 2022';
+  if(filteredYear === '2022'){
+    filterInfoText= '2019, 2020, 2021, 2023';
+  } else if(filteredYear === '2021') {
+    filterInfoText= '2019, 2020, 2022, 2023';
+  } else if(filteredYear === '2020') {
+    filterInfoText= '2019, 2021, 2022, 2023';
+  } else if(filteredYear === '2019') {
+    filterInfoText= '2020, 2021, 2022, 2023';
+  } else {
+    filterInfoText= '2019, 2020, 2021, 2022';
+  }
+  
   return (
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {filterInfoText+ ' check'}
       {expenses.map((expense) => (
         <ExpenseItem
           id={expense.id}
